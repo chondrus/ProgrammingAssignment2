77e7b28c68f9c07ce476c6385d31bbbf72f04a2d
chondrus <chondrus@users.noreply.github.com>
2014-06-22 17:12:11 -0400

    switching from tabs to spaces

--- cachematrix.R
+++ cachematrix.R
@@ -8,9 +8,9 @@
 # assigned homework ones
 # 
 # Useful note:
-# 	<<- operator
-#		assigns a value to an object in an environment
-# 		DIFFERENT from the current environment. 
+#   <<- operator
+#       assigns a value to an object in an environment
+#       DIFFERENT from the current environment. 
 #
 ###############################################################################
 
@@ -25,43 +25,43 @@
 # function: makeCacheMatrix
 #
 # takes:
-#	- matrix
+#   - matrix
 #
 # returns:
-#	-  a special "matrix"; a list containing functions to
-# 		- set a matrix
-# 		- get a matrix
-# 		- set the inverse of a matrix
-# 		- get the inverse of a matrix
+#   -  a special "matrix"; a list containing functions to
+#       - set a matrix
+#       - get a matrix
+#       - set the inverse of a matrix
+#       - get the inverse of a matrix
 #
 ################################################################################
 makeCacheMatrix <- function(x = matrix()) {
-	
-	# initialize the saved inverse to null
-	saved_inverse <- NULL
+    
+    # initialize the saved inverse to null
+    saved_inverse <- NULL
 
-	# sets x to passed in y
-	set <- function(y) {
-		x <<- y
-		saved_inverse <<- NULL
-	}
+    # sets x to passed in y
+    set <- function(y) {
+        x <<- y
+        saved_inverse <<- NULL
+    }
 
-	# returns x
-	get <- function() x
+    # returns x
+    get <- function() x
 
-	# sets saved_inverse to the passed in inverse
-	setinverse <- function(inverse) saved_inverse <<- inverse
+    # sets saved_inverse to the passed in inverse
+    setinverse <- function(inverse) saved_inverse <<- inverse
 
-	# returns saved_inverse
-	getinverse <- function() saved_inverse
+    # returns saved_inverse
+    getinverse <- function() saved_inverse
 
-	# all of the above function
-	list(
-		set = set,
-		get = get,
-		setinverse = setinverse,
-		getinverse = getinverse
-	)
+    # all of the above function
+    list(
+        set = set,
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse
+    )
 }
 
 ################################################################################
@@ -69,38 +69,38 @@ makeCacheMatrix <- function(x = matrix()) {
 # function: cacheSolve
 #
 # takes:
-#	- special "matrix" (created with the above makeCacheMatrix function).
+#   - special "matrix" (created with the above makeCacheMatrix function).
 #
 # returns:
-# 	- inverse of that matrix
+#   - inverse of that matrix
 #
 # does:
-#	- checks to see if the inverse has already been calculated
-#		- if so, returns that
+#   - checks to see if the inverse has already been calculated
+#       - if so, returns that
 #
-#	- otherwise: 
-#		- calculates the inverse
-#		- sets the value of the inverse in the cache via the setinverse function
+#   - otherwise: 
+#       - calculates the inverse
+#       - sets the value of the inverse in the cache via the setinverse function
 # 
 ################################################################################
 cacheSolve <- function(x, ...) {
 
-	# check for inverse
-	inverse <- x$getinverse()
+    # check for inverse
+    inverse <- x$getinverse()
 
-	# if it exists, print a message and then return it
-	if(!is.null(inverse)) {
-		message("getting cached data")
-		return(inverse)
-	}
+    # if it exists, print a message and then return it
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
 
-	# collect the data, calculate the inverse of it
-	data <- x$get()
-	inverse <- solve(data, ...)
+    # collect the data, calculate the inverse of it
+    data <- x$get()
+    inverse <- solve(data, ...)
 
-	# save that inverse and return it
-	x$setinverse(inverse)
-	inverse
+    # save that inverse and return it
+    x$setinverse(inverse)
+    inverse
 }
 
 ################################################################################
@@ -116,40 +116,40 @@ cacheSolve <- function(x, ...) {
 # example from class
 #
 # creates a special "vector", 
-#	which is really a list containing a function to
-# 		- set the value of the vector
-# 		- get the value of the vector
-# 		- set the value of the mean
-# 		- get the value of the mean
+#    which is really a list containing a function to
+#        - set the value of the vector
+#        - get the value of the vector
+#        - set the value of the mean
+#        - get the value of the mean
 # 
 ################################################################################
 makeVector <- function(x = numeric()) {
-	
-	# initialize the saved mean to null
-	saved_mean <- NULL
+    
+    # initialize the saved mean to null
+    saved_mean <- NULL
 
-	# set function; sets x to passed in y
-	set <- function(y) {
-		x <<- y
-		saved_mean <<- NULL
-	}
+    # set function; sets x to passed in y
+    set <- function(y) {
+        x <<- y
+        saved_mean <<- NULL
+    }
 
-	# get funcion; returns x
-	get <- function() x
+    # get funcion; returns x
+    get <- function() x
 
-	# setmean function; sets saved_mean to the passed in mean
-	setmean <- function(mean) saved_mean <<- mean
+    # setmean function; sets saved_mean to the passed in mean
+    setmean <- function(mean) saved_mean <<- mean
 
-	# get mean; returns saved_mean
-	getmean <- function() saved_mean
+    # get mean; returns saved_mean
+    getmean <- function() saved_mean
 
-	# all of the above function
-	list(
-		set = set,
-		get = get,
-		setmean = setmean,
-		getmean = getmean
-	)
+    # all of the above function
+    list(
+        set = set,
+        get = get,
+        setmean = setmean,
+        getmean = getmean
+    )
 }
 
 ################################################################################
@@ -159,34 +159,34 @@ makeVector <- function(x = numeric()) {
 # example from class
 #
 # takes:
-#	- special "vector" (created with the above makeVector function).
+#   - special "vector" (created with the above makeVector function).
 #
 # returns:
-# 	- mean of argument
+#   - mean of argument
 #
 # does:
-#	- checks to see if the mean has already been calculated
-#		- if so, returns that
-#	- calculates the mean of the data
-#	- sets the value of the mean in the cache via the setmean function
+#   - checks to see if the mean has already been calculated
+#       - if so, returns that
+#   - calculates the mean of the data
+#   - sets the value of the mean in the cache via the setmean function
 #
 ################################################################################
 cachemean <- function(x, ...) {
 
-	# check for mean (has _ to avoid confusion with the function "mean")
-	mean_ <- x$getmean()
+    # check for mean (has _ to avoid confusion with the function "mean")
+    mean_ <- x$getmean()
 
-	# if exists, print a message and then return the found mean
-	if(!is.null(mean_)) {
-		message("getting cached data")
-		return(mean_)
-	}
+    # if exists, print a message and then return the found mean
+    if(!is.null(mean_)) {
+        message("getting cached data")
+        return(mean_)
+    }
 
-	# collect the data, calculate the mean of it
-	data <- x$get()
-	mean_ <- mean(data, ...)
+    # collect the data, calculate the mean of it
+    data <- x$get()
+    mean_ <- mean(data, ...)
 
-	# save that mean and return it
-	x$setmean(mean_)
-	mean_
+    # save that mean and return it
+    x$setmean(mean_)
+    mean_
 }
